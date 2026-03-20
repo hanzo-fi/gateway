0038979bc6bee910f150c04cf88d2f790a4973ad
Maxence Maireaux <maxence@maireaux.fr>
2026-03-20 09:55:21 +0100

    fix(deps): update go-libs/v3 to v3.6.1 (#99)
    The previous pseudo-version (v3.5.1-0.20251113095923-6ac8ca3970b2)
    referenced a commit that no longer exists, breaking go mod tidy and CI.
    
    Update to the latest released version v3.6.1 and adapt the call to
    auth.ClaimsFromRequest which now expects a map[string]oidc.KeySet.

--- pkg/plugins/audit.go
+++ pkg/plugins/audit.go
@@ -459,7 +459,9 @@ func (a Audit) ServeHTTP(w http.ResponseWriter, r *http.Request, next caddyhttp.
 		if issuer == "" {
 			issuer = a.AuthURL
 		}
-		claims, tokenValidationError = auth.ClaimsFromRequest(r, issuer, a.keySet)
+		claims, tokenValidationError = auth.ClaimsFromRequest(r, map[string]oidc.KeySet{
+			issuer: a.keySet,
+		})
 	}
 
 	requestHeaders := r.Header.Clone()
